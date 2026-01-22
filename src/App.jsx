a1962e3b4fe5b70fd5cdc937b951108e5a4b5af7
saverio mandarano <saverio.mandarano1@gmail.com>
2026-01-22 15:16:35 +0100

    add MainContent.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import MainHeader from "./components/MainHeader"
 import Language from "./components/Language"
+import MainContent from "./components/MainContent"
 
 function App() {
 
   return (
     <>
       <MainHeader />
-      <Language />
+      <MainContent />
     </>
   )
 }
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+import Language from "./Language";
+
+export default function MainContent () {
+    return (
+        <main>
+            <Language />
+        </main>
+    )
+}
